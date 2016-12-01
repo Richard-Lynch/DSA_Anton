d0ff925f66774d31a2f2c9e206ed2480a203b522
Anton Gerdelan <capnramses@users.noreply.github.com>
2016-12-01 21:13:28 +0000

    Create main.c

---
+++ demos/astar/main.c
@@ -0,0 +1,14 @@
+//
+// A-Star Demo for class
+// Anton Gerdelan - 1 Dec 2016
+
+// plan:
+// make a 2d grid for the graph domain
+// use manhattan distance for the heuristic
+// use the image loader we wrote to write numbered images out at fatter size than 1px
+// show grey gradient to indicate cost
+// start cell green
+// end cell red
+// path blue
+// random obstacles in black (C-shaped walls)
+
